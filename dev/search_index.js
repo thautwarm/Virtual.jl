c177671b29aaa0f58394a6f3c60bf21a8486ef2b
Documenter.jl <documenter@juliadocs.github.io>
2022-07-17 23:34:35 +0000

    build based on d68e70c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Virtual","category":"page"},{"location":"#Virtual","page":"Home","title":"Virtual","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Virtual.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Virtual]","category":"page"},{"location":"#Virtual.replace_field-Tuple{Any}","page":"Home","title":"Virtual.replace_field","text":"a slow replace and copy function used in compile time to reduce compilation latency.\n\n\n\n\n\n","category":"method"}]
+}
